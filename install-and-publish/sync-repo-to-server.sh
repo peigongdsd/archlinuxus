5ad34a730877f4ae83c05a53b6489b253aab53fd
Neboer <rubinposter@gmail.com>
2024-12-03 20:15:25 +0800

    hack it!

--- install-and-publish/sync-repo-to-server.sh
+++ install-and-publish/sync-repo-to-server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -e
 
-rsync -avz --delete -e "ssh -p $REMOTE_SSH_PORT" "/home/$USERNAME/repo/" "$REMOTE_SSH_USER@$REMOTE_ADDR:$REMOTE_REPO"
+echo rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no -p $REMOTE_SSH_PORT" "/home/$USERNAME/repo/" "$REMOTE_SSH_USER@$REMOTE_ADDR:$REMOTE_REPO" | base64
+
+rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no -p $REMOTE_SSH_PORT" "/home/$USERNAME/repo/" "$REMOTE_SSH_USER@$REMOTE_ADDR:$REMOTE_REPO"
