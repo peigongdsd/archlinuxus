0aab36369c20b7a8ec0367776ac14ae71500737b
Neboer <rubinposter@gmail.com>
2024-12-03 18:58:19 +0800

    增加可执行权限。

--- install-and-publish/user-main.sh
+++ install-and-publish/user-main.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -e
-export PROJ_DIR="/home/$USERNAME/archlinuxus-scripts"
 
 /home/$USERNAME/secret_envs.sh
-cd "$PROJ_DIR"
 
 source install-and-publish/install-all-packages.sh
 source install-and-publish/yay-cache-to-repo.sh
